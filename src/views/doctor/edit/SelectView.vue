10eb37b093341c8d5a1a7ee3763de8acac50a27e
glebanya13 <gleb2000405@outlook.com>
2022-12-09 04:40:26 +0300

    select

--- src/views/doctor/edit/SelectView.vue
+++ src/views/doctor/edit/SelectView.vue
@@ -3,49 +3,66 @@
         <div class="edit-block">
             <div class="edit-block__registration-block">
                 <div class="edit-fields">
-                    <h1 class="edit-registration-block__title">Выберите тип вопроса</h1>
-                    <div class="ul">
-                        <div class="li">
-                            <div class="image">
-                                <div class="icon">
-                                    <img class="logo" src="../../../assets/edit/select.png" alt="select" />
-                                </div>
-                            </div>
-                            <div class="data">
-                                <div class="select">С вариантами</div>
-                            </div>
-                        </div>
-                        <div class="li">
-                            <div class="image">
-                                <div class="icon">
-                                    <img class="logo" src="../../../assets/edit/str.png" alt="str" />
-                                </div>
-                            </div>
-                            <div class="data">
-                                <div class="select">Текстовый </div>
-                            </div>
-                        </div>
-                        <div class="li">
-                            <div class="image">
-                                <div class="icon">
-                                    <img class="logo" src="../../../assets/edit/int.png" alt="int" />
-                                </div>
-                            </div>
-                            <div class="data">
-                                <div class="select">Численный</div>
-                            </div>
-                        </div>
+                    <h1 class="edit-registration-block__title">Введите вопрос</h1>
+
+                    <div class="form-field">
+                        <input type="text" placeholder="">
                     </div>
-                    <button class="button edit-btn">
+
+                    <button class="button str-btn">
+                        Закончить
+                    </button>
+                    <button class="button str-btn">
                         <router-link to="/reports">Вернуться</router-link>
                     </button>
 
+
                 </div>
             </div>
-            <div class="edit-block__logo-block">
-                <div class="logo-block-img">
-                    <img src="../../../assets/edit/logo.png">
+            <div class="edit-block__logo-block" v-if="(items.length == 2)">
+                <div class="edit-blocks" v-for="item in items" :key="item.id">
+                    <div class="edit-block__banner">
+                        <div class="edit-fields">
+                            <div class="form-field">
+                                <input type="text" v-model="item.title" placeholder="Введите вариант">
+                            </div>
+                        </div>
+
+                        <div class="icons">
+                            <div class="icon">
+                                <img src="../../../assets/edit/close.png" alt="close">
+                            </div>
+                            <div class="icon">
+                                <img src="../../../assets/edit/success.png" alt="success">
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div class="edit-block__logo-block-1" v-if="(items.length > 2)">
+                <div class="edit-blocks" v-for="item in items" :key="item.id">
+                    <div class="edit-block__banner-1">
+                        <div class="edit-fields">
+                            <div class="form-field">
+                                <input type="text" v-model="item.title" placeholder="Введите вариант">
+                            </div>
+                        </div>
+
+                        <div class="icons">
+                            <div class="icon" @click="destroy(item.id)">
+                                <img src="../../../assets/edit/close.png" alt="close">
+                            </div>
+                            <div class="icon">
+                                <img src="../../../assets/edit/success.png" alt="success">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="edit-block_plus" @click="add">
+                <img src="../../../assets/edit/plus.png" alt="close">
             </div>
         </div>
     </div>
@@ -56,6 +73,35 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'StrView',
+    data() {
+        return {
+            items: [
+                { id: 0, title: '' }, { id: 1, title: '' }
+            ]
+        }
+    },
+    methods: {
+        add() {
+            if (this.items.length < 4) {
+                this.items.push({
+                    id: this.items.length - 1,
+                    title: ''
+                });
+            }
+            return;
+        },
+        destroy(id: number) {
+            console.log(id);
+
+            let index = this.items.findIndex(i => i.id == id);
+
+            if (this.items.length > 2) {
+                this.items.splice(index, 1);
+            }
+
+            return;
+        }
+    }
 });
 </script>
   
